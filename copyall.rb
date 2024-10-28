366956ccf862d5356ab495480b707a4b81484e6f
Olawale Akinseye <brainyfarm@gmail.com>
2024-10-28 20:21:08 +0000

    Change hash

--- copyall.rb
+++ copyall.rb
@@ -2,7 +2,7 @@ class Copyall < Formula
   desc "CopyAll - A customizable file copying script"
   homepage "https://github.com/brainyfarm/homebrew-copyall"
   url "https://github.com/brainyfarm/homebrew-copyall/releases/download/v.0.3.2/copyall-0.3.2.tar.gz"
-  sha256 "837b6dec8e4560f335720fba080a00dc10c22ffbdd67fdb66b90cf1b222bcec0"
+  sha256 "3af48c042aeb5b19abe151e574e9b4877ec59f8664138f365c439dc6eb685f6e"
   license "MIT"
 
   def install
