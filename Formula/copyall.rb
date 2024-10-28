01d38cfb5cbc372d728e931b0665921448cdad78
Olawale Akinseye <brainyfarm@gmail.com>
2024-10-28 19:36:41 +0000

    Update release URL

--- Formula/copyall.rb
+++ Formula/copyall.rb
@@ -1,7 +1,7 @@
 class Copyall < Formula
     desc "CopyAll - A customizable file copying script"
     homepage "https://github.com/brainyfarm/homebrew-copyall"
-    url "https://github.com/brainyfarm/homebrew-copyall/archive/refs/tags/v0.3.0.tar.gz"
+    url "https://github.com/brainyfarm/homebrew-copyall/releases/download/v0.3.0/copyall-0.3.0.tar.gz"
     sha256 "01d270a2c78df5b63b149e9dfb726ce0a4f660d8abec9f12a6f93a149111f8dd"
     license "MIT"
   
