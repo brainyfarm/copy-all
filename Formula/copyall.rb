12c0d9441691239177f5400b7624828b53890ecd
Olawale Akinseye <brainyfarm@gmail.com>
2024-10-28 19:41:48 +0000

    fix install dir

--- Formula/copyall.rb
+++ Formula/copyall.rb
@@ -1,19 +1,19 @@
 class Copyall < Formula
-    desc "CopyAll - A customizable file copying script"
-    homepage "https://github.com/brainyfarm/homebrew-copyall"
-    url "https://github.com/brainyfarm/homebrew-copyall/releases/download/v0.3.0/copyall-0.3.0.tar.gz"
-    sha256 "01d270a2c78df5b63b149e9dfb726ce0a4f660d8abec9f12a6f93a149111f8dd"
-    license "MIT"
-  
-    depends_on "bash"
-  
-    def install
-      bin.install "src/main.sh" => "copyall"
-      prefix.install "src"
-    end
-  
-    test do
-      assert_match "Usage:", shell_output("#{bin}/copyall --help")
-    end
+  desc "CopyAll - A customizable file copying script"
+  homepage "https://github.com/brainyfarm/homebrew-copyall"
+  url "https://github.com/brainyfarm/homebrew-copyall/releases/download/v0.3.0/copyall-0.3.0.tar.gz"
+  sha256 "01d270a2c78df5b63b149e9dfb726ce0a4f660d8abec9f12a6f93a149111f8dd"
+  license "MIT"
+
+  depends_on "bash"
+
+  def install
+    libexec.install Dir["src/*"]
+
+    (bin/"copyall").write_env_script libexec/"main.sh", PATH: "#{libexec}:$PATH"
   end
-  
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/copyall --help")
+  end
+end
